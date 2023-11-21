845637906d763e790cec9491a68e6790dec14f72
Dieyna22 <101632839+Dieyna22@users.noreply.github.com>
2023-11-21 16:23:36 +0000

    second commit

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArticleServiceService } from '../service/article-service.service';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.css']
 })
-export class ArticlesComponent {
+export class ArticlesComponent  implements OnInit {
+
+  recupArticle: any;
+  constructor(private articlesService: ArticleServiceService) {}
+  
+  searchArticle = '';
+  itemSearch: any;
+  
+  ngOnInit() {
+    this.itemSearch = this.recupArticle;
+    this.articlesService.getArticles().subscribe((articles: any) => {
+      this.recupArticle = articles;
+    })
+}
+ 
+  articleFound() {
+    this.itemSearch = this.recupArticle.filter(
+      (item: any) => (item?.title.toLowerCase().includes(this.searchArticle.toLowerCase())));
+  }
 
 }
